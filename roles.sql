db07c6ff2fa399eea41f23d4f2172e696cebb941
JhonatanD09 <you@example.com>
2022-12-03 18:00:12 -0500

    roles

---
+++ roles.sql
@@ -0,0 +1,10 @@
+--Este script solo se puede ejecutar en administradores
+CREATE ROLE vendedor;
+CREATE ROLE administrador;
+
+
+--Permisos de rol vendedor en las tablas
+GRANT SELECT,INSERT,UPDATE ON HR.ZAP_TALL_COL TO vendedor;
+GRANT SELECT,INSERT,UPDATE ON HR.FACTURAS TO vendedor;
+GRANT SELECT,INSERT,UPDATE ON HR.DETALLES TO vendedor;
+GRANT SELECT,INSERT,UPDATE ON HR.CONTROL_LOG TO vendedor;
